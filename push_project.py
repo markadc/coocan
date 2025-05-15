3e80503b8f4d17a0961f2245594aa704fdf1ef45
markadc <markadc@126.com>
2025-05-15 22:56:29 +0800

    支持代理；加入默认超时6秒；优化代码

--- push_project.py
+++ push_project.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-msg = sys.argv[1] if len(sys.argv) == 2 else "update readme"
+msg = sys.argv[1] if len(sys.argv) == 2 else "支持代理；加入默认超时6秒；优化代码"
 
 cmd1 = "git add ."
 cmd2 = 'git commit -m "{}"'.format(msg)
--- url/request.py
+++ url/request.py
@@ -7,22 +7,37 @@ cli = httpx.AsyncClient()
 
 
 class Request:
-    def __init__(self, url: str, callback: Callable = None, params=None, headers=None, data=None, json=None, timeout=None, cb_kwargs=None, priority=None):
+    def __init__(
+            self,
+            url: str, callback: Callable = None, cb_kwargs=None,
+            params=None, headers=None,
+            data=None, json=None,
+            proxy=None, timeout=6,
+            priority=None
+    ):
         self.url = url
         self.callback = callback
+        self.cb_kwargs = cb_kwargs or {}
         self.params = params
         self.headers = headers or {}
         self.data = data
         self.json = json
+        self.proxy = proxy
         self.timeout = timeout
-        self.cb_kwargs = cb_kwargs or {}
         self.priority = priority or time.time()
+        self.__client = None
+
+    @property
+    def client(self):
+        if self.proxy is None:
+            return cli
+        return httpx.AsyncClient(proxy=self.proxy)
 
     async def send(self):
         if (self.data and self.json) is None:
-            response = await cli.get(self.url, params=self.params, headers=self.headers, timeout=self.timeout)
+            response = await self.client.get(self.url, params=self.params, headers=self.headers, timeout=self.timeout)
         elif self.data or self.json:
-            response = await cli.post(self.url, params=self.params, headers=self.headers, data=self.data, json=self.json, timeout=self.timeout)
+            response = await self.client.post(self.url, params=self.params, headers=self.headers, data=self.data, json=self.json, timeout=self.timeout)
         else:
             raise Exception("仅支持 GET 和 POST 请求")
         return response
